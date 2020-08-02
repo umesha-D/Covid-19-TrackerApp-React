a12eed0927be2d6988327106ba3906fdb6099bb3
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-03 04:24:12 +0800

    async problem and materialze error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      countries: [],
+      country: "worldwide",
+    };
+  }
+
+  async componentDidMount() {
+    setTimeout(() => {
+      const elems = document.querySelectorAll("select");
+      M.FormSelect.init(elems, {});
+      this.setState({ isLoading: false });
+    }, 3000);
+
+    const call = await fetch("https://disease.sh/v3/covid-19/countries");
+    const result = await call.json();
+
+    const countries = result.map( (country) => {
+      return {
+        name:  country.country,
+        id:    country.countryInfo._id,
+        value: country.countryInfo.iso2,
+      }
+    })
+    this.setState({countries:countries});
+  }
+
+   getCountry = async (e) => {
+     await this.setState({country: e.target.value});
+     await console.log(this.state.country)
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <h3>COVID-19 TRACKER</h3>
+
+        <div className="input-field col s12">
+          <select onChange={this.getCountry} >
+            <option selected value="worldwide">World Wide</option>;
+            {this.state.countries.map((country, index) => {
+              return <option key={index}  value={country.value}>{country.name}</option>;
+            })}
+          </select>
+        </div>
+        
+
+
+      </div>
+    );
+  };
 }
 
 export default App;
